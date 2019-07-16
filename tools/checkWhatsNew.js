6eb7e30347a15b34e11151a8c23294baf490bf25
sebavan <vandenberghe.sebastien@gmail.com>
2019-07-17 01:16:16 +0200

    Introduce whats new + checks

---
+++ tools/checkWhatsNew.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const https = require("https");
+
+/**
+ * Tests the whats new file to ensure changes have been made in the PR.
+ */
+
+// Check status on azure
+if (!process.env["AZURE_PULLREQUESTID"]) {
+    console.log("Not a PR, no need to check.")
+    return;
+}
+
+// Compare what's new with the current one in the preview release folder.
+const url = "https://rawgit.com/BabylonJS/Controls/master/dist/what's%20new.md";
+https.get(url, res => {
+    res.setEncoding("utf8");
+    let oldData = "";
+    res.on("data", data => {
+        oldData += data;
+    });
+    res.on("end", () => {
+        fs.readFile("./what's new.md", "utf-8", function(err, newData) {
+            console.log(newData)
+            if (err || oldData != newData) {
+                done();
+                return;
+            }
+
+            console.error("What's new file did not change.");
+            process.exit(1);
+        });
+    });
+});
---
+++ what's new.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+## Major updates
+- First release of the [Controls Library](https://doc.babylonjs.com/features/controls) ([Deltakosh](https://github.com/deltakosh/))
+
+## Controls
+
+### Timeline
+- Scrollable Timeline control for videos [Doc](https://doc.babylonjs.com/features/timeline) / [Sebavan](https://github.com/sebavan/))
