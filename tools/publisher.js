0f6530e0d32d3c25e12210c4409a2fcc802f0b61
sebavan <vandenberghe.sebastien@gmail.com>
2019-07-31 03:37:34 +0200

    Auto version

---
+++ tools/publisher.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+exec("npm view @babylonjs/controls dist-tags.preview", (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(stdout);
+
+  const version = stdout;
+  const spl = version.split(".");
+  spl[spl.length -1]++;
+  const newVersion = spl.join(".");
+
+  console.log(newVersion);
+
+  const packageText = fs.readFileSync("package.json");
+
+  const packageJSON = JSON.parse(packageText);
+  packageJSON.version = newVersion;
+
+  fs.writeFileSync("package.json", JSON.stringify(packageJSON, null, 4));
+});
+
