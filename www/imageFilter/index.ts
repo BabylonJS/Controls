5336f3f9b72731afb3dca9ccda0f5ed2c989e048
sebavan <vandenberghe.sebastien@gmail.com>
2019-08-30 14:43:21 +0200

    Add more examples on the demo

--- www/imageFilter/index.ts
+++ www/imageFilter/index.ts
@@ -6,6 +6,7 @@ import { ImageProcessingPostProcess } from "@babylonjs/core/PostProcesses/imageP
 import { ImageProcessingConfiguration } from "@babylonjs/core/Materials/imageProcessingConfiguration";
 
 import { EffectWrapper } from "@babylonjs/core/Materials/effectRenderer";
+import { Texture } from "@babylonjs/core/Materials/Textures/texture";
 
 const beforePicture1 = document.getElementById("beforePicture1") as HTMLImageElement;
 const beforePicture2 = document.getElementById("beforePicture2") as HTMLImageElement;
@@ -40,7 +41,12 @@ function main() {
             // Samplers
             varying vec2 vUV;
             uniform sampler2D textureSampler;
-            
+
+            uniform sampler2D otherTexture;
+            uniform vec3 colorOffset;
+
+            const vec2 scale = vec2(0.25, 1.);
+
             void main(void) 
             {
                 gl_FragColor = texture2D(textureSampler, vUV);
@@ -49,15 +55,39 @@ function main() {
                 float r = gl_FragColor.r;
                 gl_FragColor.r = gl_FragColor.b;
                 gl_FragColor.b = r;
+                gl_FragColor.rgb += clamp(colorOffset, 0., 1.);
+
+                gl_FragColor.rgb *= texture2D(otherTexture, vUV * scale).rgb;
             }
         `,
-        samplerNames: ["textureSampler"]
+        // Defines the list of existing samplers.
+        samplerNames: ["textureSampler", "otherTexture"],
+        // Defines the list of existing uniform to be bound.
+        uniformNames: ["colorOffset"],
+    });
+
+    var time = 0;
+    var otherTexture = new Texture("../assets/timeline.png", customFilter.engine);
+
+    // Demos a dynamic way of using the effect over time.
+    customFilter.engine.runRenderLoop(() => {
+        // Only render if the texture is ready
+        if (!otherTexture.isReady()) {
+            return;
+        }
+
+        // Sets custom values.
+        time += customFilter.engine.getDeltaTime() / 1000;
+        customEffectWrapper.effect.setTexture("otherTexture", otherTexture);
+        customEffectWrapper.effect.setFloat3("colorOffset", Math.cos(time) * 0.5 + 0.5, 0, Math.sin(time) * 0.5 + 0.5);
+
+        // Render.
+        customFilter.filter(imageToProcess, customEffectWrapper);
     });
 
     startProcessingButton.addEventListener("click", function(e) {
         backAndWhiteFilter.filter(imageToProcess, blackAndWhitePostProcess);
         imageProcessingFilter.filter(imageToProcess, imageProcessingPostProcess);
-        customFilter.filter(imageToProcess, customEffectWrapper);
 
         e.preventDefault();
         e.stopPropagation();
